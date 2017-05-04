f9497daa22ac3cbf4606010469f9a61d1523a7ef
Derek Marsh <derekm224@gmail.com>
2017-05-04 14:35:20 -0400

    twiml Response in base views for testing number.

--- flask_twiml/base/views.py
+++ flask_twiml/base/views.py
@@ -1,6 +1,7 @@
 """
 """
 from flask import render_template
+from twilio import twiml
 
 from . import base
 
@@ -9,3 +10,11 @@ from . import base
 #@base.route('/main')
 def main():
     return render_template('base.html')
+
+
+@base.route('/twilio', methods = ['GET', 'POST'])
+def twilio():
+    """Respond to SMS with SMS"""
+    resp = twiml.Response()
+    resp.message("Hello from flask_twiml!")
+    return str(resp)
